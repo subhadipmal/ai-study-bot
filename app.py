b7250be4f4f6de28c78721aa9d86b0d63fa3cdbc
subhadipmal <dipedutech@gmail.com>
2026-03-12 20:47:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import wikipedia
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "AI Study Bot running successfully"}
+
+@app.get("/ask")
+def ask(question: str):
+    try:
+        answer = wikipedia.summary(question, sentences=2)
+        return {"question": question, "answer": answer}
+    except:
+        return {"answer": "Sorry, I couldn't find the answer."}
